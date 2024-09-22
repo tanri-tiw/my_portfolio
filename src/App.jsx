a52aef27f76a11a5472fd53fd630b43c29f1b6e5
tanri-tiw <pranjaltiwari291@gmail.com>
2024-09-22 23:42:03 +0530

    add: i hope this works

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Port />} />
+        <Route path="/my_portfolio" element={<Port />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
